1caa64ae6bcb404aa89374b480fc4a9abf0906f0
Ilya Yudin <yudin2402@gmail.com>
2025-01-30 19:49:13 +0100

    preview component copleted

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const shareBtn = document.getElementById('share-btn');
+const socials = document.getElementById('socials');
+const ACTIVE = 'active';
+
+shareBtn.addEventListener('click', toggleSocials)
+
+function toggleSocials() {
+  if(socials.classList.contains(ACTIVE)) {
+    socials.classList.remove(ACTIVE);
+  } else {
+    socials.classList.add(ACTIVE);
+  }
+}
+
+document.addEventListener("click", function (event) {
+  if(!shareBtn.contains(event.target) && !socials.contains(event.target)) {
+    socials.classList.remove(ACTIVE);
+  }
+});
